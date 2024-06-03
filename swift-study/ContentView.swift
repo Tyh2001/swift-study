daf6f64f89dd4dfa39aaa09dc1ee5c2ba16f1ea3
Tyh2001 <1469442737@qq.com>
2024-06-03 19:43:25 +0800

    Initial Commit

---
+++ swift-study/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swift-study
+//
+//  Created by Tyh2001 on 2024/6/3.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swift-study/swift_studyApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_studyApp.swift
+//  swift-study
+//
+//  Created by Tyh2001 on 2024/6/3.
+//
+
+import SwiftUI
+
+@main
+struct swift_studyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
